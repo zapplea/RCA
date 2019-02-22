c594a37c47f8c12fc10deff994bcf3bea6527f28
yibing <zapplea@gmail.com>
2019-02-23 00:57:33 +0800

    add

---
+++ sbatch.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --get-user-env
+#SBATCH --job-name="RCA"
+#SBATCH --time=24:00:00
+#SBATCH --nodes=1
+#SBATCH --mem=100GB
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+module load matlab/R2018b
+module load perl/5.24.0
+
+perl randomTF_target.pl
