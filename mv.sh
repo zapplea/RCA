231f5ebea8f644d94eb03e9eb88fcfbf478827b3
yibing <zapplea@gmail.com>
2019-02-25 14:20:47 +0800

    add

--- mv.sh
+++ mv.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# mv output[0-9]*.txt /datastore/liu121/RCA_result/RCA_12up_5TF &&
-mv 12up_output[0-9]* /datastore/liu121/RCA_result/RCA_12up_5TF
+mv output[0-9]*.txt /datastore/liu121/RCA_result/RCA_12down_5TF &&
+mv 12down_output[0-9]* /datastore/liu121/RCA_result/RCA_12down_5TF
